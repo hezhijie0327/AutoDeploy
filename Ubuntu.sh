92f274d912d08592e8d8fef8bdd355798ae13f7e
Zhijie He <hezhijie0327@hotmail.com>
2021-08-11 23:09:58 +0800

    Ubuntu.sh | Version 1.9.0
    1. <Change> Update $PATH.

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.8.9
+# Current Version: 1.9.0
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/Ubuntu.sh" | sudo bash
@@ -315,7 +315,7 @@ function ConfigurePackages() {
             "export DEBIAN_FRONTEND=\"noninteractive\""
             "export EDITOR=\"nano\""
             "export GPG_TTY=\$\(tty\)"
-            "export PATH=\"/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin\""
+            "export PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\""
             "export ZSH=\"\$HOME/.oh-my-zsh\""
             "plugins=(zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting)"
             "ZSH_CACHE_DIR=\"\$ZSH/cache\""
