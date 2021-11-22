c050abbcac3a9c72bb99aacfdbc5f98110ae6036
Zhijie He <hezhijie0327@hotmail.com>
2021-11-22 22:58:15 +0800

    ProxmoxVE.sh | Version 1.2.6
    1. <Change> Add IOMMU Interrupt Remapping patch. Please remove the # to  allow unsafe interrupts.

--- ProxmoxVE.sh
+++ ProxmoxVE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.2.5
+# Current Version: 1.2.6
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/ProxmoxVE.sh" | sudo bash
@@ -26,6 +26,9 @@ function GetSystemInformation() {
         else
             CPU_VENDOR_ID="Unknown"
             ENABLE_IOMMU=""
+        fi
+        if [ "${ENABLE_IOMMU}" != "" ]; then
+            echo "# options vfio_iommu_type1 allow_unsafe_interrupts=1" > "/etc/modprobe.d/iommu_unsafe_interrupts.conf"
         fi
     }
     function GetLSBCodename() {
@@ -197,8 +200,8 @@ function ConfigurePackages() {
         if [ -f "/etc/modules" ]; then
             rm -rf "/etc/modules"
         fi && rm -rf "/tmp/module.autodeploy" && for module_list_task in "${!module_list[@]}"; do
-                echo "${module_list[$module_list_task]}" >> "/tmp/module.autodeploy"
-            done && cat "/tmp/module.autodeploy" > "/etc/modules" && rm -rf "/tmp/module.autodeploy"
+            echo "${module_list[$module_list_task]}" >> "/tmp/module.autodeploy"
+        done && cat "/tmp/module.autodeploy" > "/etc/modules" && rm -rf "/tmp/module.autodeploy"
     }
     function ConfigurePostfix() {
         if [ -f "/etc/postfix/main.cf" ]; then
