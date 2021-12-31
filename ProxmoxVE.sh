75d4c682f76263fe10c71de7d065b7a2badf85ed
Zhijie He <hezhijie0327@hotmail.com>
2022-01-01 00:11:35 +0800

    ProxmoxVE.sh | Version 1.3.7
    1. <Change> Fix an error.

--- ProxmoxVE.sh
+++ ProxmoxVE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.3.6
+# Current Version: 1.3.7
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/ProxmoxVE.sh" | sudo bash
@@ -180,7 +180,7 @@ function ConfigurePackages() {
     }
     function ConfigureCrontab() {
         crontab_list=(
-            "0 0 * * 7 sudo apt update && sudo apt -t ${LSBCodename}-backports dist-upgrade -yq && sudo apt -t ${LSBCodename}-backports upgrade -yq && sudo apt autoremove -yq"
+            "0 0 * * 7 sudo apt update && sudo apt dist-upgrade -qy && sudo apt -t ${LSBCodename}-backports dist-upgrade -qy && sudo apt upgrade -qy && sudo apt -t ${LSBCodename}-backports upgrade -qy && sudo apt autoremove -qy"
             "@reboot sudo rm -rf /root/.*_history"
         )
         which "crontab" > "/dev/null" 2>&1
@@ -501,7 +501,7 @@ function InstallDependencyPackages() {
 }
 # Upgrade Packages
 function UpgradePackages() {
-    apt update && apt -t ${LSBCodename}-backports dist-upgrade -qy && apt -t ${LSBCodename}-backports upgrade -qy && apt autoremove -qy
+    apt update && apt dist-upgrade -qy && apt -t ${LSBCodename}-backports dist-upgrade -qy && apt upgrade -qy && apt -t ${LSBCodename}-backports upgrade -qy && apt autoremove -qy
 }
 # Cleanup Temp Files
 function CleanupTempFiles() {
