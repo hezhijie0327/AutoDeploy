375abb6438f969f73cb2420ab181e6058ffcd563
Zhijie He <hezhijie0327@hotmail.com>
2022-01-23 07:25:54 +0800

    Ubuntu.sh | Version 2.7.5
    1. <Change> Let default user as Container administrator.

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 2.7.4
+# Current Version: 2.7.5
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/Ubuntu.sh" | sudo bash
@@ -661,7 +661,7 @@ function ConfigureSystem() {
                 "@reboot rm -rf /home/${DEFAULT_USERNAME}/.*_history"
             )
             userdel -rf "${DEFAULT_USERNAME}" > "/dev/null" 2>&1
-            useradd -d "/home/${DEFAULT_USERNAME}" -s "/bin/zsh" -m "${DEFAULT_USERNAME}" && echo $DEFAULT_USERNAME:$DEFAULT_PASSWORD | chpasswd && adduser "${DEFAULT_USERNAME}" "sudo"
+            useradd -d "/home/${DEFAULT_USERNAME}" -s "/bin/zsh" -m "${DEFAULT_USERNAME}" && echo $DEFAULT_USERNAME:$DEFAULT_PASSWORD | chpasswd && adduser "${DEFAULT_USERNAME}" "docker" && adduser "${DEFAULT_USERNAME}" "sudo"
             if [ -d "/etc/zsh/oh-my-zsh" ]; then
                 cp -rf "/etc/zsh/oh-my-zsh" "/home/${DEFAULT_USERNAME}/.oh-my-zsh" && chown -R $DEFAULT_USERNAME:$DEFAULT_USERNAME "/home/${DEFAULT_USERNAME}/.oh-my-zsh"
                 if [ -f "/etc/zsh/oh-my-zsh.zshrc" ]; then
