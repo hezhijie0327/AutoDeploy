ad470268d885cfbfdf990d601d97f3989e86896a
Zhijie He <hezhijie0327@hotmail.com>
2022-02-12 08:34:03 +0800

    macOS.sh | Version 2.1.6
    1. <Change> Add support to set DNS in WireGuard.

--- macOS.sh
+++ macOS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 2.1.5
+# Current Version: 2.1.6
 
 ## How to get and use?
 # /bin/bash -c "$(curl -fsSL 'https://source.zhijie.online/AutoDeploy/main/macOS.sh')"
@@ -156,6 +156,7 @@ function ConfigurePackages() {
             wireguard_list=(
                 "[Interface]"
                 "Address = ${TUNNEL_CLIENT_V4}, ${TUNNEL_CLIENT_V6}"
+                "# DNS = 127.0.0.1"
                 "ListenPort = 51820"
                 "PrivateKey = $(wg genkey | tee '/tmp/wireguard.autodeploy')"
                 "# [Peer]"
