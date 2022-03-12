9cb331cc986add8cdb9d19e97afe7a0a4b764d32
Zhijie He <hezhijie0327@hotmail.com>
2022-03-12 17:54:45 +0800

    macOS.sh | Version 2.3.0
    1. <Change> Fix an error.

--- macOS.sh
+++ macOS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 2.2.9
+# Current Version: 2.3.0
 
 ## How to get and use?
 # /bin/bash -c "$(curl -fsSL 'https://source.zhijie.online/AutoDeploy/main/macOS.sh')"
@@ -83,7 +83,7 @@ function ConfigurePackages() {
         fi
         which "gpg" > "/dev/null" 2>&1
         if [ "$?" -eq "0" ]; then
-            rm -rf "/Users/${CurrentUsername}/.gnupg" && gpg --keyserver hkps://keys.openpgp.org --recv ${GPG_PUBKEY} && echo "${GPG_PUBKEY}" | awk 'BEGIN { FS = "\n" }; { print $1":6:" }' | gpg --import-ownertrust && GPG_PUBKEY_ID_A=$(gpg --list-keys --keyid-format LONG | grep "pub\|sub" | awk '{print $2, $4}' | grep "\[A\]" | awk '{print $1}' | awk -F '/' '{print $2}') && GPG_PUBKEY_ID_C=$(gpg --list-keys --keyid-format LONG | grep "pub\|sub" | awk '{print $2, $4}' | grep "\[C\]" | awk '{print $1}' | awk -F '/' '{print $2}')
+            rm -rf "/Users/${CurrentUsername}/.gnupg" && gpg --keyserver hkps://keys.openpgp.org --recv ${GPG_PUBKEY} && gpg --keyserver hkps://keyserver.ubuntu.com --recv ${GPG_PUBKEY} && echo "${GPG_PUBKEY}" | awk 'BEGIN { FS = "\n" }; { print $1":6:" }' | gpg --import-ownertrust && GPG_PUBKEY_ID_A=$(gpg --list-keys --keyid-format LONG | grep "pub\|sub" | awk '{print $2, $4}' | grep "\[A\]" | awk '{print $1}' | awk -F '/' '{print $2}') && GPG_PUBKEY_ID_C=$(gpg --list-keys --keyid-format LONG | grep "pub\|sub" | awk '{print $2, $4}' | grep "\[C\]" | awk '{print $1}' | awk -F '/' '{print $2}')
             if [ "${GPG_PUBKEY_ID_A}" != "" ]; then
                 if [ "${ARM_ARCHITECTURE}" == "TRUE" ]; then
                     PINENTRY_PROGRAM_PATH="/opt/homebrew/bin"
