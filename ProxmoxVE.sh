38b69b7ab3f327ff1e6c37107a41b383ea790822
Zhijie He <hezhijie0327@hotmail.com>
2022-08-23 21:24:03 +0800

    ProxmoxVE.sh | Version 2.1.5
    1. <Change> Add ethtool.

--- ProxmoxVE.sh
+++ ProxmoxVE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 2.1.4
+# Current Version: 2.1.5
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/ProxmoxVE.sh" | sudo bash
@@ -184,7 +184,7 @@ function ConfigurePackages() {
     }
     function ConfigureCrontab() {
         crontab_list=(
-            "*/15 * * * * sudo bash '/etc/pve/watchdog.sh'"
+            "*/10 * * * * sudo bash '/etc/pve/watchdog.sh'"
             "0 0 * * 7 sudo apt update && sudo apt full-upgrade -qy && sudo apt -t ${LSBCodename}-backports full-upgrade -qy && sudo apt autoremove -qy"
             "@reboot sudo rm -rf /root/.*_history /root/.ssh/known_hosts*"
         )
@@ -748,6 +748,7 @@ function InstallDependencyPackages() {
         "chrony"
         "curl"
         "dnsutils"
+        "ethtool"
         "fail2ban"
         "git"
         "git-flow"
