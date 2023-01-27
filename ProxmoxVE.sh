c9db449ce4e6ac767b2b2306bbb071c17daa04c7
Zhijie He <hezhijie0327@hotmail.com>
2023-01-27 22:30:54 +0800

    ProxmoxVE.sh | Version 3.0.9

--- ProxmoxVE.sh
+++ ProxmoxVE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 3.0.8
+# Current Version: 3.0.9
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/ProxmoxVE.sh" | sudo bash
@@ -526,7 +526,7 @@ function ConfigurePackages() {
                     UPSD_PASSWORD=$(echo "${upsd_user_list[$upsd_user_list_task]}" | cut -d ',' -f 2)
                     UPSD_ROLE=$(echo "${upsd_user_list[$upsd_user_list_task]}" | cut -d ',' -f 3)
                     echo -e "[${UPSD_USERNAME}]\n    actions = SET\n    instcmds = ALL\n    password = ${UPSD_PASSWORD}\n    upsmon ${UPSD_ROLE}" >> "/tmp/upsd.users.autodeploy"
-                done && cat "/tmp/upsd.users.autodeploy" > "/etc/nut/upsd.users" && rm -rf "/tmp/upsd.users.autodeploy"
+                done && cat "/tmp/upsd.users.autodeploy" > "/etc/nut/upsd.users" && chmod 0640 "/etc/nut/upsd.users" && rm -rf "/tmp/upsd.users.autodeploy"
             }
             function Generate_upsmon_conf() {
                 upsmon_list=(
@@ -544,7 +544,7 @@ function ConfigurePackages() {
                 )
                 rm -rf "/tmp/upsmon.conf.autodeploy" && for upsmon_list_task in "${!upsmon_list[@]}"; do
                     echo "${upsmon_list[$upsmon_list_task]}" >> "/tmp/upsmon.conf.autodeploy"
-                done && cat "/tmp/upsmon.conf.autodeploy" > "/etc/nut/upsmon.conf" && rm -rf "/tmp/upsmon.conf.autodeploy"
+                done && cat "/tmp/upsmon.conf.autodeploy" > "/etc/nut/upsmon.conf" && chmod 0640 "/etc/nut/upsmon.conf" && rm -rf "/tmp/upsmon.conf.autodeploy"
             }
             function Generate_upssched_conf() {
                 upssched_conf=(
