568a95990190b469e0fecd845138eb51b308953f
Zhijie He <hezhijie0327@hotmail.com>
2023-08-12 06:51:42 +0800

    Ubuntu.sh | Version 5.0.0

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 4.9.9
+# Current Version: 5.0.0
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/Ubuntu.sh" | sudo bash
@@ -1003,7 +1003,7 @@ function ConfigurePackages() {
         fi
     }
     function ConfigureWireGuard() {
-        TUNNEL_CLIENT_V4="192.168.$(shuf -i '224-255' -n 1).$(shuf -i '1-254' -n 1)/32"
+        TUNNEL_CLIENT_V4="10.172.$(shuf -i '0-255' -n 1).$(shuf -i '0-255' -n 1)/32"
         which "bc" > "/dev/null" 2>&1
         if [ "$?" -eq "0" ]; then
             which "sha1sum" > "/dev/null" 2>&1
@@ -1032,8 +1032,8 @@ function ConfigurePackages() {
                     "Address = ${TUNNEL_CLIENT_V4}, ${TUNNEL_CLIENT_V6}"
                     "# DNS = 127.0.0.1, ::1"
                     "ListenPort = 51820"
-                    "PostDown = ufw delete allow from 192.168.224.0/19; ufw route delete allow in on wg0 out on ${WAN_INTERFACE}"
-                    "PostUp = ufw allow from 192.168.224.0/19; ufw route allow in on wg0 out on ${WAN_INTERFACE}"
+                    "PostDown = ufw delete allow from 10.172.0.0/16; ufw route delete allow in on wg0 out on ${WAN_INTERFACE}"
+                    "PostUp = ufw allow from 10.172.0.0/16; ufw route allow in on wg0 out on ${WAN_INTERFACE}"
                     "PreDown = iptables -t nat -D POSTROUTING -o ${WAN_INTERFACE} -j MASQUERADE; ip6tables -t nat -D POSTROUTING -o ${WAN_INTERFACE} -j MASQUERADE"
                     "PreUp = iptables -t nat -A POSTROUTING -o ${WAN_INTERFACE} -j MASQUERADE; ip6tables -t nat -A POSTROUTING -o ${WAN_INTERFACE} -j MASQUERADE"
                     "PrivateKey = $(wg genkey | tee '/tmp/wireguard.autodeploy')"
