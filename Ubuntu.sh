828c10ba5b09f21973843b9bd854da118cdc3869
Zhijie He <hezhijie0327@hotmail.com>
2024-05-01 17:02:58 +0800

    Ubuntu.sh | Version 5.5.8

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 5.5.7
+# Current Version: 5.5.8
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/Ubuntu.sh" | sudo bash
@@ -1321,8 +1321,14 @@ function InstallCustomPackages() {
                         "libigfxcmrt-dev"
                         "level-zero-dev"
                     )
+                    externel_file=(
+                        "https://source.zhijie.online/DockerimageBuilder/main/patch/jellyfin/intel.sh"
+                    )
                     rm -rf "/usr/share/keyrings/intel-archive-keyring.gpg" && curl -fsSL "https://repositories.intel.com/graphics/intel-graphics.key" | gpg --dearmor -o "/usr/share/keyrings/intel-archive-keyring.gpg"
                     echo "# deb [arch=${OSArchitecture} signed-by=/usr/share/keyrings/intel-archive-keyring.gpg] https://repositories.intel.com/graphics/ubuntu ${LSBCodename} arc legacy" > "/etc/apt/sources.list.d/intel.list"
+                    rm -rf "/opt/intel-patch" && mkdir -p "/opt/intel-patch" && for externel_file_task in "${!externel_file[@]}"; do
+                        wget -P "/opt/intel-patch" "${externel_file[$externel_file_task]}"
+                    done && cat "/opt/intel-patch/intel.sh" | sed "s|/opt/intel-patch|https://source.zhijie.online/DockerimageBuilder/main/patch/jellyfin|g;s/cat/curl -s/g" > "/opt/intel-patch/patch.sh" && rm -rf "/opt/intel-patch/intel.sh"
                 fi
             ;;
             nvidia)
