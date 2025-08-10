e8383082a846d99e0f2a5d28444a9de80732c612
Zhijie He <hezhijie0327@hotmail.com>
2025-08-10 14:44:20 +0800

    Ubuntu.sh | Version 5.9.9

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 5.9.8
+# Current Version: 5.9.9
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/Ubuntu.sh" | sudo bash
@@ -1376,6 +1376,7 @@ function InstallCustomPackages() {
 
         rm -rf "/usr/share/keyrings/xanmod-archive-keyring.gpg" && curl -fsSL "https://dl.xanmod.org/archive.key" | gpg --dearmor -o "/usr/share/keyrings/xanmod-archive-keyring.gpg"
         echo "deb [arch=${OSArchitecture} signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] https://deb.xanmod.org releases main non-free" > "/etc/apt/sources.list.d/xanmod.list"
+        echo "deb [arch=${OSArchitecture} signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] https://deb.xanmod.org ${LSBCodename} main non-free" >> "/etc/apt/sources.list.d/xanmod.list"
         apt update && for apt_list_task in "${!apt_list[@]}"; do
             apt-cache show ${apt_list[$apt_list_task]} && if [ "$?" -eq "0" ]; then
                 apt install -qy ${apt_list[$apt_list_task]}
