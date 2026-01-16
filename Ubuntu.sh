955cf63e36684c9b94dfad9674121f92571f3081
Zhijie He <hezhijie0327@hotmail.com>
2026-01-16 15:15:33 +0800

    Ubuntu.sh | Version 6.1.8

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 6.1.7
+# Current Version: 6.1.8
 
 ## How to get and use?
 # curl "https://source.zhijie.online/AutoDeploy/main/Ubuntu.sh" | sudo bash
@@ -915,7 +915,7 @@ function ConfigurePackages() {
             "vm.dirty_ratio = 15"
             "vm.overcommit_memory = 1"
             "vm.page-cluster = 0"
-            "vm.swappiness = 150"
+            "vm.swappiness = 10"
             ${icmp_echo[@]}
         )
         which "sysctl" > "/dev/null" 2>&1
