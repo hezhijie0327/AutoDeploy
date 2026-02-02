4adb805474c0cba61335018c8365158b79698c58
Zhijie He <hezhijie0327@hotmail.com>
2026-02-02 22:39:27 +0800

    macOS.sh | Version 2.7.5

--- macOS.sh
+++ macOS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 2.7.4
+# Current Version: 2.7.5
 
 ## How to get and use?
 # /bin/bash -c "$(curl -fsSL 'https://source.zhijie.online/AutoDeploy/main/macOS.sh')"
@@ -276,7 +276,7 @@ function ConfigurePackages() {
         }
         function GenerateOMZProfile() {
             HOMEBREW_GITHUB_API_TOKEN=""
-            PROXY_PASSWORD=""
+            PROXY_AUTH=""
             omz_list=(
                 "export EDITOR=\"nano\""
                 "export GPG_TTY=\$(tty)"
@@ -290,7 +290,7 @@ function ConfigurePackages() {
                 "# export SSH_AUTH_SOCK=\"\$(gpgconf --list-dirs agent-ssh-socket)\" && gpgconf --launch gpg-agent && gpg-connect-agent updatestartuptty /bye > \"/dev/null\" 2>&1"
                 "export ZSH=\"\$HOME/.oh-my-zsh\""
                 "function proxy_off(){ unset all_proxy; unset ftp_proxy; unset http_proxy; unset https_proxy; unset rsync_proxy }"
-                "function proxy_on(){ export all_proxy=\"socks5://${PROXY_PASSWORD:+${PROXY_PASSWORD}@}vpn.zhijie.online:7890\"; export ftp_proxy=\"http://${PROXY_PASSWORD:+${PROXY_PASSWORD}@}vpn.zhijie.online:7890\"; export http_proxy=\"http://${PROXY_PASSWORD:+${PROXY_PASSWORD}@}vpn.zhijie.online:7890\"; export https_proxy=\"http://${PROXY_PASSWORD:+${PROXY_PASSWORD}@}vpn.zhijie.online:7890\"; export rsync_proxy=\"http://${PROXY_PASSWORD:+${PROXY_PASSWORD}@}vpn.zhijie.online:7890\" }"
+                "function proxy_on(){ export all_proxy=\"socks5://${PROXY_AUTH:+${PROXY_AUTH}@}vpn.zhijie.online:7890\"; export ftp_proxy=\"http://${PROXY_AUTH:+${PROXY_AUTH}@}vpn.zhijie.online:7890\"; export http_proxy=\"http://${PROXY_AUTH:+${PROXY_AUTH}@}vpn.zhijie.online:7890\"; export https_proxy=\"http://${PROXY_AUTH:+${PROXY_AUTH}@}vpn.zhijie.online:7890\"; export rsync_proxy=\"http://${PROXY_AUTH:+${PROXY_AUTH}@}vpn.zhijie.online:7890\" }"
                 "plugins=(zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting)"
                 "ZSH_CACHE_DIR=\"\$ZSH/cache\""
                 "ZSH_CUSTOM=\"\$ZSH/custom\""
